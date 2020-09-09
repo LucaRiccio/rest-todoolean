3e020c4b6c2489cf0c24492529a95cdeee8bae1a
LucaRiccio <lucariccio888@gmail.com>
2020-09-09 17:04:26 +0200

    POST method

--- js/script.js
+++ js/script.js
@@ -6,21 +6,30 @@ $(document).ready(function(){
 
   getData();
 
-  $(document).on('click', '.delete', function(){
+  $(document).on('click', '.delete', function(){  // Utilizzato on Click perchè gli elementi sui quali operiamo sono creati dinamicamente.
     var elemento = $(this); // Salvo in una variabile il this (facoltativo, non è necessario).
-    var idToDo = elemento.parent().attr('data-id'); // Sull'elemento cliccato (this), risalgo al genitore (<li>) e ne prendo l'attributo (data-id)
-    deleteElement(idToDo);
+    var idToDo = elemento.parent().attr('data-id'); // Sull'elemento cliccato (this), risalgo al genitore (<li>) e ne prendo l'attributo (data-id).
+    deleteElement(idToDo); // Invoco una funzione in cui passo come argomento la variabile idToDo (che per come è stata creata contiene l'id).
   });
+
+  $(".inserisci").click(function(){ // Al click del pulsante inserisci
+    var newElement = $("#nuova-voce").val(); // Prendo il valore della casella input
+    createElement(newElement); // Invoco una funzione che ha per argomento il valore della casella input salvato in var.
+  })
 
 });
 
-function deleteElement(id){
+
+function createElement(elemento){
+
   $.ajax(
     {
-      url: 'http://157.230.17.132:3025/todos/' + id,
-      method: 'DELETE',
+      url: 'http://157.230.17.132:3025/todos',
+      method: 'POST',
+      data:{        // <---- necessario il data
+        text: elemento
+      },
       success: function(risposta){
-        console.log(risposta);
         $(".todos").html('');
         getData();
       },
@@ -31,8 +40,24 @@ function deleteElement(id){
   );
 }
 
+function deleteElement(id){
+  $.ajax(
+    {
+      url: 'http://157.230.17.132:3025/todos/' + id,
+      method: 'DELETE',
+      success: function(risposta){
+        $(".todos").html(''); // Svuoto il contenuto...
+        getData(); // e richiamo nuovamente la funzione per ottenere i dati.
+      },
+      error: function(){
+        alert("Errore");
+      }
+    }
+  );
+}
 
-function getData(){ // chiamata AJAX "trasformata" in funzione
+
+function getData(){ // chiamata AJAX per ottenere i dati "trasformata" in funzione
   $.ajax(
     {
       url: 'http://157.230.17.132:3025/todos', // URL dell' API
@@ -50,7 +75,7 @@ function getData(){ // chiamata AJAX "trasformata" in funzione
 function getElement(data){                  // <---- Ciò che generalmente inserisco nella funcrion del success della chiamata ma tradotta in funzione
   var source = $("#entry-template").html();
   var template = Handlebars.compile(source);
-  for (var i = 0; i < data.length; i++){
+  for (var i = 0; i < data.length; i++){ // Ciclo For per scorrere l'array.
     var context = {       // Avrei potuto usare var context = risposta[i];
       text: data[i].text,
       id: data[i].id
